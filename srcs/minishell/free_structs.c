3674141e76147f5a37229fcda97cf73539a76e9d
Valerio Duchi <vduchi@car9s6.42barcelona.com>
2023-08-12 21:23:35 +0200

    Added the last two files missing

---
+++ srcs/minishell/free_structs.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_structs.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 17:46:37 by vduchi            #+#    #+#             */
+/*   Updated: 2023/08/12 18:27:03 by vduchi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// TODO
+// cambiar nombre y mover las funciones dondes corresponda
+
+int	free_parser(t_parser *words, int out)
+{
+	t_parser	*tmp;
+	t_parser	*next;
+
+	tmp = words;
+	next = words->next;
+	while (next)
+	{
+		free(tmp->word);
+		free(tmp);
+		tmp = next;
+		next = tmp->next;
+	}
+	free(tmp->word);
+	free(tmp);
+	return (out);
+}
+
+int	free_env(t_env *first)
+{
+	t_env	*next;
+
+	printf("Free env\n");
+	next = first->next;
+	while (next)
+	{
+		free(first->name);
+		free(first->value);
+		free(first);
+		first = next;
+		next = first->next;
+	}
+	if (first->name)
+		free(first->name);
+	if (first->value)
+		free(first->value);
+	free(first);
+	return (MALLOC);
+}
+
+void	free_commands(t_cmd **first)
+{
+	t_cmd	*tmp;
+	t_cmd	*next;
+
+	printf("Free commands\n");
+	tmp = *first;
+	next = (*first)->next;
+	while (next)
+	{
+//		printf("Free while start\n\n");
+		if (tmp->cmd)
+			free(tmp->cmd);
+		if (tmp->args)
+			free_double_void(tmp->args);
+		free(tmp);
+		tmp = next;
+		next = tmp->next;
+//		printf("Free while end\n\n");
+	}
+//	printf("While commands finished\n");
+	if (tmp->cmd)
+		free(tmp->cmd);
+//	printf("Free last command\n");
+	if (tmp->args)
+		free_double_void(tmp->args);
+//	printf("Free double void\n");
+	free(tmp);
+	*first = NULL;
+}
+
+int	free_all(t_min *tk, int out)
+{
+	printf("Free tokens\n");
+	if (tk->cmds)
+		free_commands(&tk->cmds);
+	if (tk->env)
+		free_env(tk->env);
+	free_double_void(tk->path);
+	tk->path = NULL;
+	free(tk);
+	return (out);
+}
+
---
+++ srcs/minishell/take_env.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   take_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vduchi <vduchi@student.42barcelona.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 11:02:42 by vduchi            #+#    #+#             */
+/*   Updated: 2023/08/12 18:03:49 by vduchi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_env	*save_node(t_min *tk, t_env *new, t_env *old_tmp)
+{
+	t_env	*tmp;
+
+	if (!old_tmp)
+	{
+		new->before = NULL;
+		tk->env = new;
+		tmp = tk->env;
+	}
+	else
+	{
+		old_tmp->next = new;
+		old_tmp->next->before = old_tmp;
+		tmp = old_tmp->next;
+	}
+	return (tmp);
+}
+
+int	take_env(t_min *tk, char *env[])
+{
+	int		i;
+	t_env	*new;
+	t_env	*tmp;
+
+	i = -1;
+	tmp = NULL;
+	while (env[++i])
+	{
+		new = ft_calloc(1, sizeof(t_env));
+		if (!new)
+			return (MALLOC);
+		new->name = ft_substr(env[i], 0, ft_strrchr(env[i], '=') - env[i] + 1);
+		if (!ft_strncmp(env[i], "SHLVL=", 6))
+			new->value = ft_strdup(ft_itoa(ft_atoi(ft_strrchr(env[i], \
+				'=') + 1) + 1));
+		else if (!ft_strncmp(env[i], "OLDPWD=", 7))
+			new->value = NULL;
+		else
+			new->value = ft_strdup(ft_strrchr(env[i], '=') + 1);
+		if (!new->name || (ft_strncmp(new->name, "OLDPWD=", 7) && !new->value))
+			return (free_env(new));
+		tmp = save_node(tk, new, tmp);
+	}
+	tmp = tk->env;
+	while (tmp)
+	{
+		printf("Env: %s%s\n", tmp->name, tmp->value);
+		tmp = tmp->next;
+	}
+	return (0);
+}
