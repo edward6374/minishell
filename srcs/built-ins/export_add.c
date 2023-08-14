c2e40c2563630ca827c5152cda488ef875a1de2f
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-08-14 22:59:42 +0200

    limpieza

--- srcs/built-ins/export_add.c
+++ srcs/built-ins/export_add.c
@@ -6,45 +6,12 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:25:41 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/08/14 22:44:11 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/08/14 22:58:10 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built-ins.h"
 #include "libft.h"
-
-// Encuentra el valor, del nombre pasado en un lista
-
-// int	find_env(char *s1, char *s2)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (s2[i])
-// 	{
-// 		if (s1[i] == s2[i])
-// 			i++;
-// 		else
-// 			return (1);
-// 	}
-// 	if (s1[i] == '=')
-// 		return (0);
-// 	return (1);
-// }
-
-// t_env	*env_find(t_env *env, void *data_ref, int (*cmp)(char *, char *))
-// {
-// 	t_env	*curr;
-
-// 	curr = env;
-// 	while (curr)
-// 	{
-// 		if ((*cmp)(curr->name, data_ref) == 0)
-// 			return (curr);
-// 		curr = curr->next;
-// 	}
-// 	return (NULL);
-// }
 
 t_env static *new_env(char *str, char *value)
 {
