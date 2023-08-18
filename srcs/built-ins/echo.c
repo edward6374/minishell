d41889ab2a2e14d05434b050d0a51b79ee7002ca
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-08-18 17:51:51 +0200

    echo OK?

--- srcs/built-ins/echo.c
+++ srcs/built-ins/echo.c
@@ -6,35 +6,52 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 17:32:30 by vduchi            #+#    #+#             */
-/*   Updated: 2023/08/18 13:57:22 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/08/18 17:50:57 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built-ins.h"
-
-// char	*take_dollar(char *env)
-// {
-// 	if (*env == '$')
-// 		env++;
-// 	return (env);
-// }
-
-// void	print_echo(t_env *env, char *args)
-// {
-// 	t_env	*find;
-
-// 	find = env_find(env, take_dollar(args), find_env);
-// 	printf("%s", find->value);
-// }
+#include <stdbool.h>
 
 int	is_n(char *args, int *i)
 {
-	if (ft_strlen(args) == 2 && (args != NULL && ft_strncmp(args, "-n", 2) == 0))
+	int	j;
+
+	j = 2;
+	if (*i == 1 && args != NULL && !ft_strncmp(args, "-n", 2)
+		&& args[2] == '\0')
 	{
 		(*i)++;
 		return (TRUE);
 	}
+	else if (args[0] == '-' && args[1] == 'n')
+	{
+		while (args[j])
+		{
+			if (args[j] != 'n')
+				return (FALSE);
+			j++;
+		}
+		(*i)++;
+		return (TRUE);
+	}
 	return (FALSE);
+}
+
+void static	print_echo(char **args, int i, int p, int type)
+{
+	if (!type)
+	{
+		ft_putstr_fd(args[i], p);
+		if (args[i + 1])
+			ft_putchar_fd(' ', p);
+	}
+	else
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+	}
 }
 
 int	ft_echo(t_cmd *tmp, char **args, int p)
@@ -42,22 +59,30 @@ int	ft_echo(t_cmd *tmp, char **args, int p)
 	int	i;
 	int	no_newline;
 
-	i = 0;
+	i = 1;
 	no_newline = is_n(args[1], &i);
-	while (args[++i] != NULL)
+	while (args[i] != NULL)
 	{
+		if (!ft_strncmp(args[i], "-n", 3) && no_newline)
+		{
+			i++;
+			continue ;
+		}
 		if (tmp->next)
-		{
-			ft_putstr_fd(args[i], p);
-			if (args[i + 1])
-				ft_putchar_fd(' ', p);
-		}
+			print_echo(args, i, p, 0);
+		// {
+		// 	ft_putstr_fd(args[i], p);
+		// 	if (args[i + 1])
+		// 		ft_putchar_fd(' ', p);
+		// }
 		else
-		{
-			printf("%s", args[i]);
-			if (args[i + 1])
-				printf(" ");
-		}
+			print_echo(args, i, p, 1);
+		// {
+		// 	printf("%s", args[i]);
+		// 	if (args[i + 1])
+		// 		printf(" ");
+		// }
+		i++;
 	}
 	if (!no_newline && tmp->next)
 		ft_putchar_fd('\n', p);
