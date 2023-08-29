0d7df58fb5e5e1059431bbb183f0f761d9272774
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-08-29 18:53:18 +0200

    cd upgrade una funcion

--- srcs/built_ins/cd.c
+++ srcs/built_ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 17:25:00 by vduchi            #+#    #+#             */
-/*   Updated: 2023/08/29 16:23:23 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/08/29 18:41:08 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,66 +41,77 @@ int static	change_directory(const char *path)
 // }
 
 // controlar si hay OLDPWD o PWD
-void static upgrade_old(t_min *tk)
-{
-	char *pwd;
-	t_env *find;
+// void static	upgrade_old(t_min *tk)
+// {
+// 	char	*pwd;
+// 	t_env	*find;
 
-	pwd = getcwd(NULL, 0);
-	find = env_find(tk->env, "OLDPWD", find_env);
-	if (find)
-	{
-		free(find->value);
-		find->value = pwd;
-	}
-	if (tk->oldpwd != NULL)
-		free(tk->oldpwd);
-	tk->oldpwd = ft_strdup(pwd);
-}
+// 	pwd = getcwd(NULL, 0);
+// 	find = env_find(tk->env, "OLDPWD", find_env);
+// 	if (find)
+// 	{
+// 		free(find->value);
+// 		find->value = pwd;
+// 	}
+// 	if (tk->oldpwd != NULL)
+// 		free(tk->oldpwd);
+// 	tk->oldpwd = ft_strdup(pwd);
+// }
 
-void static upgrade_pwd(t_min *tk)
+// void static	upgrade_pwd(t_min *tk)
+// {
+// 	char	*pwd;
+// 	t_env	*find;
+
+// 	pwd = getcwd(NULL, 0);
+// 	find = env_find(tk->env, "PWD", find_env);
+// 	if (find)
+// 	{
+// 		free(find->value);
+// 		find->value = pwd;
+// 	}
+// 	if (tk->pwd != NULL)
+// 		free(tk->pwd);
+// 	tk->pwd = ft_strdup(pwd);
+// }
+
+void static	upgrade(t_min *tk, char *str)
 {
 	char	*pwd;
 	t_env	*find;
 
 	pwd = getcwd(NULL, 0);
-	find = env_find(tk->env, "PWD", find_env);
+	find = env_find(tk->env, str, find_env);
 	if (find)
 	{
 		free(find->value);
 		find->value = pwd;
 	}
-	if (tk->pwd != NULL)
-		free(tk->pwd);
-	tk->pwd = ft_strdup(pwd);
+	if (!ft_strncmp("PWD", str, 4))
+	{
+		if (tk->pwd != NULL)
+			free(tk->pwd);
+		tk->pwd = ft_strdup(pwd);
+	}
+	else
+	{
+		if (tk->oldpwd != NULL)
+			free(tk->oldpwd);
+		tk->oldpwd = ft_strdup(pwd);
+	}
 }
 
-// else
-// {
-// 	find = env_find(tk->env, "PWD", find_env);
-// }
-
-// char static *oldpwd(t_env *env)
-// {
-// 	t_env *find;
-
-// 	find = env_find(env, "OLDPWD", find_env);
-// 	return (find->value);
-// }
-
-int ft_cd(char **args, t_min *tk)
+int	ft_cd(char **args, t_min *tk)
 {
-	char *path;
-	int res;
+	char	*path;
+	int		res;
 
 	res = 0;
 	if (args[1] != NULL)
 	{
 		if (!ft_strncmp("~", args[1], 1))
-			// path = home_dir(args[1], tk->env);
 			path = tk->home;
 		else if (!ft_strncmp("-", args[1], 2))
-		// path = oldpwd(tk->env);
 		{
 			if (!tk->oldpwd)
 			{
@@ -111,13 +122,9 @@ int ft_cd(char **args, t_min *tk)
 		}
 		else
 			path = args[1];
-		upgrade_old(tk);
+		upgrade(tk, "OLDPWD");
 		res = change_directory(path);
-		upgrade_pwd(tk);
-		// upgrade_pwd(tk);
+		upgrade(tk, "PWD");
 	}
 	return (res);
 }
-
-// si no hay
-// bash: cd: OLDPWD not set
