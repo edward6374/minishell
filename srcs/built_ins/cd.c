40068f7c33c97573b20c73c555126bd125b6e2ab
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-08-30 11:49:31 +0200

    norm ok export, cd

--- srcs/built_ins/cd.c
+++ srcs/built_ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 17:25:00 by vduchi            #+#    #+#             */
-/*   Updated: 2023/08/29 18:41:08 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/08/30 11:28:43 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,57 +23,6 @@ int static	change_directory(const char *path)
 		return (1);
 	}
 }
-
-// char static	*home_dir(char *args, t_env *env)
-// {
-// 	t_env	*find;
-// 	char	*path;
-
-// 	find = env_find(env, "HOME", find_env);
-// 	if (ft_strlen(args) > 2)
-// 	{
-// 		args++;
-// 		path = ft_strjoin((const char *)find->value, args);
-// 		return (path);
-// 	}
-// 	else
-// 		return (find->value);
-// }
-
-// controlar si hay OLDPWD o PWD
-// void static	upgrade_old(t_min *tk)
-// {
-// 	char	*pwd;
-// 	t_env	*find;
-
-// 	pwd = getcwd(NULL, 0);
-// 	find = env_find(tk->env, "OLDPWD", find_env);
-// 	if (find)
-// 	{
-// 		free(find->value);
-// 		find->value = pwd;
-// 	}
-// 	if (tk->oldpwd != NULL)
-// 		free(tk->oldpwd);
-// 	tk->oldpwd = ft_strdup(pwd);
-// }
-
-// void static	upgrade_pwd(t_min *tk)
-// {
-// 	char	*pwd;
-// 	t_env	*find;
-
-// 	pwd = getcwd(NULL, 0);
-// 	find = env_find(tk->env, "PWD", find_env);
-// 	if (find)
-// 	{
-// 		free(find->value);
-// 		find->value = pwd;
-// 	}
-// 	if (tk->pwd != NULL)
-// 		free(tk->pwd);
-// 	tk->pwd = ft_strdup(pwd);
-// }
 
 void static	upgrade(t_min *tk, char *str)
 {
--- srcs/built_ins/export_add.c
+++ srcs/built_ins/export_add.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:25:41 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/08/29 23:35:45 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/08/30 11:49:02 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,68 +44,6 @@ int static	is_name(char *name)
 	return (1);
 }
 
-// void static	take_plus(char *str)
-// {
-// 	int	len;
-
-// 	len = strlen(str);
-// 	if (len > 0 && str[len - 1] == '+')
-// 		str[len - 1] = '\0';
-// }
-
-// void static	export_loop(t_env *env, char *name, char *value)
-// {
-// 	t_env	*find;
-
-// 	find = NULL;
-// 	if (!env_find(env, name, find_env))
-// 		env_add_back(&env, new_env(name, value));
-// 	else
-// 	{
-// 		find = env_find(env, name, find_env);
-// 		free(find->value);
-// 		find->value = value;
-// 	}
-// 	free(name);
-// 	free(value);
-// }
-
-// int	export_add(t_env *env, char **args)
-// {
-// 	int		i;
-// 	char	*name;
-// 	char	*value;
-
-// 	i = 1;
-// 	if (args[0])
-// 	{
-// 		while (args[i] != NULL)
-// 		{
-// 			name = ft_substr(args[i], 0, ft_strcspn(args[i], "="));
-// 			value = ft_substr(args[i], (ft_strlen(name) + 1), 0xFFFFFFF);
-// 			// take_plus(name);
-// 			if (!is_name(name))
-// 			{
-// 				printf(":( export: `%s\': not valid identifier\n", args[i]);
-// 				free(name);
-// 				free(value);
-// 				name = NULL;
-// 			}
-// 			if (name)
-// 				export_loop(env, name, value);
-// 			i++;
-// 		}
-// 	}
-// 	return (0);
-// }
-
-void static	change_value(t_env *env, t_export dt)
-{
-	dt.find = env_find(env, dt.name, find_env);
-	free(dt.find->value);
-	dt.find->value = dt.value;
-}
-
 void	remove_plus(char *str)
 {
 	int	len;
@@ -113,6 +51,27 @@ void	remove_plus(char *str)
 	len = strlen(str);
 	if (len > 0 && str[len - 1] == '+')
 		str[len - 1] = '\0';
+}
+
+void	export_loop(t_env *env, char **args, t_export *dt)
+{
+	if (!is_name(dt->name))
+	{
+		printf(":( export: `%s\': not valid identifier\n", args[dt->i]);
+		dt->res = 1;
+		dt->name = NULL;
+	}
+	if (dt->name != NULL)
+	{
+		if (!env_find(env, dt->name, find_env))
+			env_add_back(&env, new_env(dt->name, dt->value));
+		else
+		{
+			dt->find = env_find(env, dt->name, find_env);
+			free(dt->find->value);
+			dt->find->value = dt->value;
+		}
+	}
 }
 
 int	export_add(t_env *env, char **args)
@@ -130,19 +89,7 @@ int	export_add(t_env *env, char **args)
 			dt.value = ft_substr(args[dt.i], (ft_strlen(dt.name) + 1),
 					0xFFFFFFF);
 			remove_plus(dt.name);
-			if (!is_name(dt.name))
-			{
-				printf(":( export: `%s\': not valid identifier\n", args[dt.i]);
-				dt.res = 1;
-				dt.name = NULL;
-			}
-			if (dt.name != NULL)
-			{
-				if (!env_find(env, dt.name, find_env))
-					env_add_back(&env, new_env(dt.name, dt.value));
-				else
-					change_value(env, dt);
-			}
+			export_loop(env, args, &dt);
 			dt.i++;
 		}
 	}
